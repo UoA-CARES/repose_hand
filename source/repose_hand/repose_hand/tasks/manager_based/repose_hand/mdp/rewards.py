e4a8b50a12db2a24e17a70dfe8f9d4b8fbb27a94
leech <chamhaolee@gmail.com>
2025-08-18 16:29:56 +1200

    complete PPO allegro with original setting

--- source/repose_hand/repose_hand/tasks/manager_based/repose_hand/mdp/rewards.py
+++ source/repose_hand/repose_hand/tasks/manager_based/repose_hand/mdp/rewards.py
@@ -3,24 +3,94 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-from __future__ import annotations
+"""Functions specific to the in-hand dexterous manipulation environments."""
 
 import torch
 from typing import TYPE_CHECKING
 
-from isaaclab.assets import Articulation
+import isaaclab.utils.math as math_utils
+from isaaclab.assets import RigidObject
+from isaaclab.envs import ManagerBasedRLEnv
 from isaaclab.managers import SceneEntityCfg
-from isaaclab.utils.math import wrap_to_pi
 
 if TYPE_CHECKING:
-    from isaaclab.envs import ManagerBasedRLEnv
+    from .commands import InHandReOrientationCommand
 
 
-def joint_pos_target_l2(env: ManagerBasedRLEnv, target: float, asset_cfg: SceneEntityCfg) -> torch.Tensor:
-    """Penalize joint position deviation from a target value."""
-    # extract the used quantities (to enable type-hinting)
-    asset: Articulation = env.scene[asset_cfg.name]
-    # wrap the joint positions to (-pi, pi)
-    joint_pos = wrap_to_pi(asset.data.joint_pos[:, asset_cfg.joint_ids])
-    # compute the reward
-    return torch.sum(torch.square(joint_pos - target), dim=1)
+def success_bonus(
+    env: ManagerBasedRLEnv, command_name: str, object_cfg: SceneEntityCfg = SceneEntityCfg("object")
+) -> torch.Tensor:
+    """Bonus reward for successfully reaching the goal.
+
+    The object is considered to have reached the goal when the object orientation is within the threshold.
+    The reward is 1.0 if the object has reached the goal, otherwise 0.0.
+
+    Args:
+        env: The environment object.
+        command_name: The command term to be used for extracting the goal.
+        object_cfg: The configuration for the scene entity. Default is "object".
+    """
+    # extract useful elements
+    asset: RigidObject = env.scene[object_cfg.name]
+    command_term: InHandReOrientationCommand = env.command_manager.get_term(command_name)
+
+    # obtain the goal orientation
+    goal_quat_w = command_term.command[:, 3:7]
+    # obtain the threshold for the orientation error
+    threshold = command_term.cfg.orientation_success_threshold
+    # calculate the orientation error
+    dtheta = math_utils.quat_error_magnitude(asset.data.root_quat_w, goal_quat_w)
+
+    return dtheta <= threshold
+
+
+def track_pos_l2(
+    env: ManagerBasedRLEnv, command_name: str, object_cfg: SceneEntityCfg = SceneEntityCfg("object")
+) -> torch.Tensor:
+    """Reward for tracking the object position using the L2 norm.
+
+    The reward is the distance between the object position and the goal position.
+
+    Args:
+        env: The environment object.
+        command_term: The command term to be used for extracting the goal.
+        object_cfg: The configuration for the scene entity. Default is "object".
+    """
+    # extract useful elements
+    asset: RigidObject = env.scene[object_cfg.name]
+    command_term: InHandReOrientationCommand = env.command_manager.get_term(command_name)
+
+    # obtain the goal position
+    goal_pos_e = command_term.command[:, 0:3]
+    # obtain the object position in the environment frame
+    object_pos_e = asset.data.root_pos_w - env.scene.env_origins
+
+    return torch.norm(goal_pos_e - object_pos_e, p=2, dim=-1)
+
+
+def track_orientation_inv_l2(
+    env: ManagerBasedRLEnv,
+    command_name: str,
+    object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+    rot_eps: float = 1e-3,
+) -> torch.Tensor:
+    """Reward for tracking the object orientation using the inverse of the orientation error.
+
+    The reward is the inverse of the orientation error between the object orientation and the goal orientation.
+
+    Args:
+        env: The environment object.
+        command_name: The command term to be used for extracting the goal.
+        object_cfg: The configuration for the scene entity. Default is "object".
+        rot_eps: The threshold for the orientation error. Default is 1e-3.
+    """
+    # extract useful elements
+    asset: RigidObject = env.scene[object_cfg.name]
+    command_term: InHandReOrientationCommand = env.command_manager.get_term(command_name)
+
+    # obtain the goal orientation
+    goal_quat_w = command_term.command[:, 3:7]
+    # calculate the orientation error
+    dtheta = math_utils.quat_error_magnitude(asset.data.root_quat_w, goal_quat_w)
+
+    return 1.0 / (dtheta + rot_eps)
--- source/repose_hand/repose_hand/tasks/manager_based/repose_hand/repose_hand_env_cfg.py
+++ source/repose_hand/repose_hand/tasks/manager_based/repose_hand/repose_hand_env_cfg.py
@@ -3,10 +3,12 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import math
+from __future__ import annotations
+
+from dataclasses import MISSING
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg
 from isaaclab.envs import ManagerBasedRLEnvCfg
 from isaaclab.managers import EventTermCfg as EventTerm
 from isaaclab.managers import ObservationGroupCfg as ObsGroup
@@ -15,16 +17,19 @@ from isaaclab.managers import RewardTermCfg as RewTerm
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sim.simulation_cfg import PhysxCfg, SimulationCfg
+from isaaclab.sim.spawners.materials.physics_materials_cfg import RigidBodyMaterialCfg
 from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from isaaclab.utils.noise import AdditiveGaussianNoiseCfg as Gnoise
 
-from . import mdp
+import isaaclab_tasks.manager_based.manipulation.inhand.mdp as mdp
 
+from isaaclab_assets import ALLEGRO_HAND_CFG  # isort: skip
 ##
 # Pre-defined configs
 ##
-
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
-
+from isaaclab_assets import ALLEGRO_HAND_CFG  # isort: skip
 
 ##
 # Scene definition
@@ -33,21 +38,40 @@ from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
 
 @configclass
 class ReposeHandSceneCfg(InteractiveSceneCfg):
-    """Configuration for a cart-pole scene."""
+    """Configuration for a scene with an object and a dexterous hand."""
 
-    # ground plane
-    ground = AssetBaseCfg(
-        prim_path="/World/ground",
-        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
+    # robots
+    robot: ArticulationCfg = ALLEGRO_HAND_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+    # objects
+    object: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/object",
+        spawn=sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/DexCube/dex_cube_instanceable.usd",
+            rigid_props=sim_utils.RigidBodyPropertiesCfg(
+                kinematic_enabled=False,
+                disable_gravity=False,
+                enable_gyroscopic_forces=True,
+                solver_position_iteration_count=8,
+                solver_velocity_iteration_count=0,
+                sleep_threshold=0.005,
+                stabilization_threshold=0.0025,
+                max_depenetration_velocity=1000.0,
+            ),
+            mass_props=sim_utils.MassPropertiesCfg(density=400.0),
+        ),
+        init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, -0.19, 0.56), rot=(1.0, 0.0, 0.0, 0.0)),
     )
 
-    # robot
-    robot: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-
     # lights
+    light = AssetBaseCfg(
+        prim_path="/World/light",
+        spawn=sim_utils.DistantLightCfg(color=(0.95, 0.95, 0.95), intensity=1000.0),
+    )
+
     dome_light = AssetBaseCfg(
-        prim_path="/World/DomeLight",
-        spawn=sim_utils.DomeLightCfg(color=(0.9, 0.9, 0.9), intensity=500.0),
+        prim_path="/World/domeLight",
+        spawn=sim_utils.DomeLightCfg(color=(0.02, 0.02, 0.02), intensity=1000.0),
     )
 
 
@@ -57,10 +81,30 @@ class ReposeHandSceneCfg(InteractiveSceneCfg):
 
 
 @configclass
+class CommandsCfg:
+    """Command specifications for the MDP."""
+
+    object_pose = mdp.InHandReOrientationCommandCfg(
+        asset_name="object",
+        init_pos_offset=(0.0, 0.0, -0.04),
+        update_goal_on_success=True,
+        orientation_success_threshold=0.1,
+        make_quat_unique=False,
+        marker_pos_offset=(-0.2, -0.06, 0.08),
+        debug_vis=True,
+    )
+
+
+@configclass
 class ActionsCfg:
     """Action specifications for the MDP."""
 
-    joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["slider_to_cart"], scale=100.0)
+    joint_pos = mdp.EMAJointPositionToLimitsActionCfg(
+        asset_name="robot",
+        joint_names=[".*"],
+        alpha=0.95,
+        rescale_to_limits=True,
+    )
 
 
 @configclass
@@ -68,43 +112,147 @@ class ObservationsCfg:
     """Observation specifications for the MDP."""
 
     @configclass
-    class PolicyCfg(ObsGroup):
-        """Observations for policy group."""
+    class KinematicObsGroupCfg(ObsGroup):
+        """Observations with full-kinematic state information.
+
+        This does not include acceleration or force information.
+        """
 
         # observation terms (order preserved)
-        joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
-        joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
+        # -- robot terms
+        joint_pos = ObsTerm(func=mdp.joint_pos_limit_normalized, noise=Gnoise(std=0.005))
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel, scale=0.2, noise=Gnoise(std=0.01))
 
-        def __post_init__(self) -> None:
-            self.enable_corruption = False
+        # -- object terms
+        object_pos = ObsTerm(
+            func=mdp.root_pos_w, noise=Gnoise(std=0.002), params={"asset_cfg": SceneEntityCfg("object")}
+        )
+        object_quat = ObsTerm(
+            func=mdp.root_quat_w, params={"asset_cfg": SceneEntityCfg("object"), "make_quat_unique": False}
+        )
+        object_lin_vel = ObsTerm(
+            func=mdp.root_lin_vel_w, noise=Gnoise(std=0.002), params={"asset_cfg": SceneEntityCfg("object")}
+        )
+        object_ang_vel = ObsTerm(
+            func=mdp.root_ang_vel_w,
+            scale=0.2,
+            noise=Gnoise(std=0.002),
+            params={"asset_cfg": SceneEntityCfg("object")},
+        )
+
+        # -- command terms
+        goal_pose = ObsTerm(func=mdp.generated_commands, params={"command_name": "object_pose"})
+        goal_quat_diff = ObsTerm(
+            func=mdp.goal_quat_diff,
+            params={"asset_cfg": SceneEntityCfg("object"), "command_name": "object_pose", "make_quat_unique": False},
+        )
+
+        # -- action terms
+        last_action = ObsTerm(func=mdp.last_action)
+
+        def __post_init__(self):
+            self.enable_corruption = True
             self.concatenate_terms = True
 
+    @configclass
+    class NoVelocityKinematicObsGroupCfg(KinematicObsGroupCfg):
+        """Observations with partial kinematic state information.
+
+        In contrast to the full-kinematic state group, this group does not include velocity information
+        about the robot joints and the object root frame. This is useful for tasks where velocity information
+        is not available or has a lot of noise.
+        """
+
+        def __post_init__(self):
+            # call parent post init
+            super().__post_init__()
+            # set unused terms to None
+            self.joint_vel = None
+            self.object_lin_vel = None
+            self.object_ang_vel = None
+
     # observation groups
-    policy: PolicyCfg = PolicyCfg()
+    policy: KinematicObsGroupCfg = KinematicObsGroupCfg()
 
 
 @configclass
 class EventCfg:
-    """Configuration for events."""
+    """Configuration for randomization."""
 
-    # reset
-    reset_cart_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
+    # startup
+    # -- robot
+    robot_physics_material = EventTerm(
+        func=mdp.randomize_rigid_body_material,
+        mode="startup",
         params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
-            "position_range": (-1.0, 1.0),
-            "velocity_range": (-0.5, 0.5),
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+            "static_friction_range": (0.7, 1.3),
+            "dynamic_friction_range": (0.7, 1.3),
+            "restitution_range": (0.0, 0.0),
+            "num_buckets": 250,
+        },
+    )
+    robot_scale_mass = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+            "mass_distribution_params": (0.95, 1.05),
+            "operation": "scale",
+        },
+    )
+    robot_joint_stiffness_and_damping = EventTerm(
+        func=mdp.randomize_actuator_gains,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
+            "stiffness_distribution_params": (0.3, 3.0),  # default: 3.0
+            "damping_distribution_params": (0.75, 1.5),  # default: 0.1
+            "operation": "scale",
+            "distribution": "log_uniform",
         },
     )
 
-    reset_pole_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
+    # -- object
+    object_physics_material = EventTerm(
+        func=mdp.randomize_rigid_body_material,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("object", body_names=".*"),
+            "static_friction_range": (0.7, 1.3),
+            "dynamic_friction_range": (0.7, 1.3),
+            "restitution_range": (0.0, 0.0),
+            "num_buckets": 250,
+        },
+    )
+    object_scale_mass = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("object"),
+            "mass_distribution_params": (0.4, 1.6),
+            "operation": "scale",
+        },
+    )
+
+    # reset
+    reset_object = EventTerm(
+        func=mdp.reset_root_state_uniform,
         mode="reset",
         params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
-            "position_range": (-0.25 * math.pi, 0.25 * math.pi),
-            "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
+            "pose_range": {"x": [-0.01, 0.01], "y": [-0.01, 0.01], "z": [-0.01, 0.01]},
+            "velocity_range": {},
+            "asset_cfg": SceneEntityCfg("object", body_names=".*"),
+        },
+    )
+    reset_robot_joints = EventTerm(
+        func=mdp.reset_joints_within_limits_range,
+        mode="reset",
+        params={
+            "position_range": {".*": [0.2, 0.2]},
+            "velocity_range": {".*": [0.0, 0.0]},
+            "use_default_offset": True,
+            "operation": "scale",
         },
     )
 
@@ -113,41 +261,51 @@ class EventCfg:
 class RewardsCfg:
     """Reward terms for the MDP."""
 
-    # (1) Constant running reward
-    alive = RewTerm(func=mdp.is_alive, weight=1.0)
-    # (2) Failure penalty
-    terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
-    # (3) Primary task: keep pole upright
-    pole_pos = RewTerm(
-        func=mdp.joint_pos_target_l2,
-        weight=-1.0,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
+    # -- task
+    # track_pos_l2 = RewTerm(
+    #     func=mdp.track_pos_l2,
+    #     weight=-10.0,
+    #     params={"object_cfg": SceneEntityCfg("object"), "command_name": "object_pose"},
+    # )
+    track_orientation_inv_l2 = RewTerm(
+        func=mdp.track_orientation_inv_l2,
+        weight=1.0,
+        params={"object_cfg": SceneEntityCfg("object"), "rot_eps": 0.1, "command_name": "object_pose"},
     )
-    # (4) Shaping tasks: lower cart velocity
-    cart_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.01,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
+    success_bonus = RewTerm(
+        func=mdp.success_bonus,
+        weight=250.0,
+        params={"object_cfg": SceneEntityCfg("object"), "command_name": "object_pose"},
     )
-    # (5) Shaping tasks: lower pole angular velocity
-    pole_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.005,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
-    )
+
+    # -- penalties
+    joint_vel_l2 = RewTerm(func=mdp.joint_vel_l2, weight=-2.5e-5)
+    action_l2 = RewTerm(func=mdp.action_l2, weight=-0.0001)
+    action_rate_l2 = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
+
+    # -- optional penalties (these are disabled by default)
+    # object_away_penalty = RewTerm(
+    #     func=mdp.is_terminated_term,
+    #     weight=-0.0,
+    #     params={"term_keys": "object_out_of_reach"},
+    # )
 
 
 @configclass
 class TerminationsCfg:
     """Termination terms for the MDP."""
 
-    # (1) Time out
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
-    # (2) Cart out of bounds
-    cart_out_of_bounds = DoneTerm(
-        func=mdp.joint_pos_out_of_manual_limit,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
+
+    max_consecutive_success = DoneTerm(
+        func=mdp.max_consecutive_success, params={"num_success": 50, "command_name": "object_pose"}
     )
+
+    object_out_of_reach = DoneTerm(func=mdp.object_away_from_robot, params={"threshold": 0.3})
+
+    # object_out_of_reach = DoneTerm(
+    #     func=mdp.object_away_from_goal, params={"threshold": 0.24, "command_name": "object_pose"}
+    # )
 
 
 ##
@@ -158,10 +316,23 @@ class TerminationsCfg:
 @configclass
 class ReposeHandEnvCfg(ManagerBasedRLEnvCfg):
     # Scene settings
-    scene: ReposeHandSceneCfg = ReposeHandSceneCfg(num_envs=4096, env_spacing=4.0)
+    scene: ReposeHandSceneCfg = ReposeHandSceneCfg(num_envs=8192, env_spacing=0.6)
+    # Simulation settings
+    sim: SimulationCfg = SimulationCfg(
+        physics_material=RigidBodyMaterialCfg(
+            static_friction=1.0,
+            dynamic_friction=1.0,
+        ),
+        physx=PhysxCfg(
+            bounce_threshold_velocity=0.2,
+            gpu_max_rigid_contact_count=2**20,
+            gpu_max_rigid_patch_count=2**23,
+        ),
+    )
     # Basic settings
     observations: ObservationsCfg = ObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
+    commands: CommandsCfg = CommandsCfg()
     events: EventCfg = EventCfg()
     # MDP settings
     rewards: RewardsCfg = RewardsCfg()
@@ -171,10 +342,55 @@ class ReposeHandEnvCfg(ManagerBasedRLEnvCfg):
     def __post_init__(self) -> None:
         """Post initialization."""
         # general settings
-        self.decimation = 2
-        self.episode_length_s = 5
-        # viewer settings
-        self.viewer.eye = (8.0, 0.0, 5.0)
+        self.decimation = 4
+        self.episode_length_s = 20.0
         # simulation settings
-        self.sim.dt = 1 / 120
+        self.sim.dt = 1.0 / 120.0
         self.sim.render_interval = self.decimation
+        # change viewer settings
+        self.viewer.eye = (2.0, 2.0, 2.0)
+        # switch robot to allegro hand
+        # self.scene.robot = ALLEGRO_HAND_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        # enable clone in fabric
+        # self.scene.clone_in_fabric = True
+
+
+@configclass
+class ReposeHandEnvCfg_PLAY(ReposeHandEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+        # make a smaller scene for play
+        self.scene.num_envs = 50
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
+        # remove termination due to timeouts
+        self.terminations.time_out = None
+
+
+##
+# Environment configuration with no velocity observations.
+##
+
+
+@configclass
+class ReposeHandNoVelObsEnvCfg(ReposeHandEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        # switch observation group to no velocity group
+        self.observations.policy = ObservationsCfg.NoVelocityKinematicObsGroupCfg()
+
+
+@configclass
+class ReposeHandNoVelObsEnvCfg_PLAY(ReposeHandNoVelObsEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+        # make a smaller scene for play
+        self.scene.num_envs = 50
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
+        # remove termination due to timeouts
+        self.terminations.time_out = None
